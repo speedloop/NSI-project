b43aa5f2177aa3ec09b9d81459ed70ebc512a4d2
speedloop <77881914+speedloop@users.noreply.github.com>
2022-01-17 08:55:17 +0100

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,4 +1,10 @@
+#definition de constantes
+
 taille_cases = 37     
+hauteur_personnage = 20
+largeur_personnage = 19
 
 def constantes():
     global taille_cases
+    global hauteur_personnage
+    global largeur_personnage
