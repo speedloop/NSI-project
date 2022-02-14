c75632e347de17da5e3925a8c50d4b10d2a001e0
speedloop <77881914+speedloop@users.noreply.github.com>
2022-02-14 15:27:25 +0100

    Add files via upload

--- sources/settings.py
+++ sources/settings.py
@@ -4,13 +4,15 @@ from sources.constantes import *
 def settings(screen):
     screen.fill((80,80,80))    
 
-    pygame.draw.rect(screen, (0,0,0),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),border_radius=15)
+    change_char_rect = pygame.draw.rect(screen, (0,0,0),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),border_radius=15)
     pygame.draw.rect(screen, (255,255,255),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),1,border_radius=15)
     change_char_surf = settings_buttons_font.render("Change Character",1,(255,255,255))
     pos_text = ((1300/2 - 2*marge_buttons_settings)/2 - change_char_surf.get_width()//2 + marge_buttons_settings , 2*marge_buttons_settings + 30 - change_char_surf.get_height()//2)
     screen.blit(change_char_surf,pos_text)
 
     pygame.display.update()
+
+    change_char_selected = False
 
     continuer = True
     while(continuer):
@@ -19,3 +21,22 @@ def settings(screen):
                 return False
             if event.type == pygame.KEYDOWN:
                 return True
+
+            if event.type == pygame.MOUSEMOTION:
+                if change_char_rect.collidepoint(pygame.mouse.get_pos()): #souris sur bouton changement de personnage
+                    change_char_selected = True
+                else: change_char_selected = False
+
+
+        screen.fill((80,80,80))
+
+        pygame.draw.rect(screen, (0,0,0),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),border_radius=15)
+        if change_char_selected :#affichage du bouton en vert clair si la souris est au-dessus
+            pygame.draw.rect(screen, (0,255,0),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),1,border_radius=15)
+            change_char_surf = settings_buttons_font.render("Change Character",1,(0,255,0))
+        else: #affichage du bouton normal, en blanc
+            pygame.draw.rect(screen, (255,255,255),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),1,border_radius=15)
+            change_char_surf = settings_buttons_font.render("Change Character",1,(255,255,255))
+        screen.blit(change_char_surf,pos_text)
+
+        pygame.display.update()
