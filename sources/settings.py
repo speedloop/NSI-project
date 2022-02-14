c81f81fd3c526cbe060fd5fd223174e7735e7b5c
Chatauscours <95481182+Chatauscours@users.noreply.github.com>
2022-02-14 21:33:20 +0100

    Update settings.py

--- sources/settings.py
+++ sources/settings.py
@@ -5,6 +5,7 @@ def settings(screen):
     screen.fill((80,80,80))    
 
     change_char_rect = pygame.draw.rect(screen, (0,0,0),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),border_radius=15)
+    pygame.draw.rect(screen, (255,255,255),(marge_buttons_settings,2*marge_buttons_settings,1300/2 - 2*marge_buttons_settings,60),1,border_radius=15)
     change_char_surf = settings_buttons_font.render("Change Character",1,(255,255,255))
     pos_text = ((1300/2 - 2*marge_buttons_settings)/2 - change_char_surf.get_width()//2 + marge_buttons_settings , 2*marge_buttons_settings + 30 - change_char_surf.get_height()//2)
     screen.blit(change_char_surf,pos_text)
